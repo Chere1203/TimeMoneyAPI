7deef63bf05246325ed0909f7b7ce454401a7642
Erick Durán <ejoduma12@gmail.com>
2025-11-20 16:07:59 -0600

    Arreglo de server.js para azure

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-import "./src/app.js";
+import app from "./src/app.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`API escuchando en http://0.0.0.0:${PORT}`);
+});
